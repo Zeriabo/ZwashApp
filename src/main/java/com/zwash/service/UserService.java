a61dfc7978e66cfd1b30a442f3ea135d568abec2
Zeriab Chiah <zeriab@hotmail.com>
2023-04-24 16:51:17 +0300

    user Service reformat

--- src/main/java/com/zwash/service/UserService.java
+++ src/main/java/com/zwash/service/UserService.java
@@ -1,23 +1,22 @@
 package com.zwash.service;
 
 import java.io.Serializable;
-import java.sql.SQLException;
-
-import org.springframework.stereotype.Service;
 
 import com.zwash.exceptions.UserIsNotFoundException;
 import com.zwash.pojos.LoggedUser;
 import com.zwash.pojos.User;
-	
-	
-	public interface UserService extends Serializable  {
-		
-		 LoggedUser signIn(String username, String password) throws Exception;
-		 User register(User user) throws Exception;
-		 boolean changePassword(String username, String password) throws  Exception;
-		 boolean validateSignIn(String token);
-		 User getSecretQuestionAnswer(String username);
-		 User getUser(long id) throws UserIsNotFoundException;
-	
-		
-	}
+
+public interface UserService extends Serializable {
+    
+    LoggedUser signIn(String username, String password) throws Exception;
+    
+    User register(User user) throws Exception;
+    
+    boolean changePassword(String username, String password) throws Exception;
+    
+    boolean validateSignIn(String token);
+    
+    User getSecretQuestionAnswer(String username);
+    
+    User getUser(long id) throws UserIsNotFoundException;
+}
