4faaec5951359610e56e66fd3bab21cf405ba994
Zeriab Chiah <zeriab@hotmail.com>
2023-04-24 15:52:30 +0300

    refactor carcontroler

--- src/main/java/com/zwash/controller/CarController.java
+++ src/main/java/com/zwash/controller/CarController.java
@@ -5,14 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zwash.exceptions.IncorrectTokenException;
 import com.zwash.exceptions.UserIsNotFoundException;
 import com.zwash.pojos.Car;
@@ -22,72 +21,57 @@ import com.zwash.security.JwtUtils;
 import com.zwash.service.CarService;
 import com.zwash.service.UserService;
 
-
 import io.jsonwebtoken.Claims;
 
-@Controller
-@RequestMapping(value = "/cars")
+@RestController
+@RequestMapping("/cars")
 public class CarController {
 
 	@Autowired
-	private CarService carService;
-	
+	CarService carService;
 	@Autowired
-	private UserService userService;
+	UserService userService;
 
-	@PostMapping("/resgistercar")
-	public ResponseEntity<Car> resgisterCar(@RequestBody String carInfo) throws Exception {
-
-		ObjectMapper mapper = new ObjectMapper();
-
-		UserCar car = mapper.readValue(carInfo, UserCar.class);
-
-		Car newcar = carService.register(car);
-		if (newcar instanceof Car) {
-			return new ResponseEntity<Car>(HttpStatus.ACCEPTED);
-		} else {
-			return new ResponseEntity<Car>(HttpStatus.NOT_ACCEPTABLE);
-		}
-
+	public CarController(CarService carService, UserService userService) {
+		this.carService = carService;
+		this.userService = userService;
 	}
-	
-	@GetMapping("/{registerationPlate}")
-	public Car getCar(@PathVariable("registerationPlate") String registerationPlate) {
-		return carService.getCar(registerationPlate);
+
+	@PostMapping("/register")
+	public ResponseEntity<Void> registerCar(@RequestBody UserCar userCar) throws Exception {
+		Car car = carService.register(userCar);
+		return car instanceof Car ? ResponseEntity.accepted().build() : ResponseEntity.status(500).build();
 	}
-	
+
+	@GetMapping("/{registrationPlate}")
+	public ResponseEntity<Car> getCar(@PathVariable String registrationPlate) {
+		Car car = carService.getCar(registrationPlate);
+		return car != null ? ResponseEntity.ok(car) : ResponseEntity.notFound().build();
+	}
+
 	@GetMapping("/user/{username}")
-	public List<Car> getCarsOfUser(@PathVariable("username") String username) throws UserIsNotFoundException {
+	public ResponseEntity<List<Car>> getCarsForUser(@PathVariable String username) throws UserIsNotFoundException {
 		User user = new User();
 		user.setUsername(username);
-		return carService.getCarsOfUser(user);
+		List<Car> cars = carService.getCarsOfUser(user);
+		return ResponseEntity.ok(cars);
 	}
-	
-	@PostMapping("/setcar")
-	public ResponseEntity<Car> setCar(@RequestBody UserCar userCar) throws Exception {
-	
-		 JwtUtils jwtUtils = new JwtUtils();
-			try {
-				String registerationPlate=userCar.getRegisterationPlate();
-				Claims cl =jwtUtils.verifyJWT(userCar.getToken());
-		     
-				Car car =carService.getCar(registerationPlate);
-				User user =userService.getUser(Long.parseLong(cl.getId()));
-				System.out.print(cl);
-			    car.setUser(user);
-			    
-			   if( carService.updateCar(car))
-			   {
-				   return new ResponseEntity<Car>(HttpStatus.ACCEPTED);
-			   }else {
-				   return new ResponseEntity<Car>(HttpStatus.NOT_ACCEPTABLE);
-			   }
-			}catch(Exception ex)
-			{
-				throw new IncorrectTokenException("The token is not valid!");
+
+	@PostMapping("/set")
+	public ResponseEntity<Void> setCar(@RequestBody UserCar userCar) throws IncorrectTokenException {
+		try {
+			String registrationPlate = userCar.getRegisterationPlate();
+			Claims claims = new JwtUtils().verifyJWT(userCar.getToken());
+			Car car = carService.getCar(registrationPlate);
+			User user = userService.getUser(Long.parseLong(claims.getId()));
+			car.setUser(user);
+			if (carService.updateCar(car)) {
+				return ResponseEntity.accepted().build();
+			} else {
+				return ResponseEntity.status(500).build();
 			}
-		
-
-
-}
+		} catch (Exception ex) {
+			throw new IncorrectTokenException("The token is not valid!");
+		}
+	}
 }
