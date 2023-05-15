d90b9623d8ad595adfadf5bb159a17f6ea1b1c2f
Zeriab Chiah <zeriab@hotmail.com>
2023-05-15 11:48:39 +0300

    booking swagger

--- src/main/java/com/zwash/controller/BookingController.java
+++ src/main/java/com/zwash/controller/BookingController.java
@@ -25,11 +25,16 @@ import com.zwash.pojos.User;
 import com.zwash.service.BookingService;
 import com.zwash.service.CarService;
 import com.zwash.service.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.Api;
 import jakarta.transaction.Transactional;
 import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("v1/bookings")
+@Api(tags = "Booking API")
 public class BookingController {
 	@Autowired
 	private CarService carService;
@@ -42,7 +47,12 @@ public class BookingController {
     Logger logger = LoggerFactory.getLogger(BookingController.class);
 
 
-	@GetMapping(value="/{id}" ,produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value="/{id}")
+	@ApiOperation(value = "Get a booking by ID", response = Booking.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved booking"),
+			@ApiResponse(code = 404, message = "Booking not found")
+	})
 	public ResponseEntity<Booking> getBooking(@PathVariable Long id) {
     	Booking booking = bookingService.getBookingById(id);
 		if (booking != null) {
@@ -53,7 +63,12 @@ public class BookingController {
 	}
 
  
-	@GetMapping
+    @GetMapping
+	@ApiOperation(value = "Get all bookings", response = BookingDTO.class, responseContainer = "List")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved bookings"),
+			@ApiResponse(code = 404, message = "Bookings not found")
+	})
 	public  ResponseEntity<List<BookingDTO>>getAllBookings() throws Exception {
     	try {
     		List<BookingDTO> list= bookingService.getAllBookings();
@@ -65,8 +80,13 @@ public class BookingController {
 	}
 
   
-	@PostMapping
+    @PostMapping
 	@Transactional
+	@ApiOperation(value = "Create a booking", response = Booking.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Successfully created booking"),
+			@ApiResponse(code = 400, message = "Invalid request")
+	})
 	public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) throws UserIsNotFoundException {
 		if (booking == null) {
 			throw new IllegalArgumentException("Booking  cannot be null");
@@ -101,6 +121,12 @@ public class BookingController {
 	}
 	@PutMapping("/{id}")
 	@Transactional
+	@ApiOperation(value = "Update an existing booking", response = Booking.class)
+	@ApiResponses(value = {
+	@ApiResponse(code = 200, message = "Booking updated successfully"),
+	@ApiResponse(code = 400, message = "Invalid request. Check input parameters"),
+	@ApiResponse(code = 404, message = "Booking with provided id not found")
+	})
 	public ResponseEntity<Booking> updateBooking(@PathVariable Long id, @RequestBody Booking newBooking) {
 		if (newBooking == null) {
 			throw new IllegalArgumentException("Booking object cannot be null");
@@ -132,6 +158,11 @@ public class BookingController {
 
    @DeleteMapping("/{id}")
 	@Transactional
+	@ApiOperation(value = "Delete a booking by id")
+	@ApiResponses(value = {
+	@ApiResponse(code = 204, message = "Booking deleted successfully"),
+	@ApiResponse(code = 404, message = "Booking with provided id not found")
+	})
 	public ResponseEntity<Void> deleteBooking(@PathVariable Long id) {
 		Booking booking = bookingService.getBookingById(id);
 		if (booking != null) {
@@ -142,6 +173,11 @@ public class BookingController {
 		}
 	}
 	@GetMapping("validate/{registrationPlate}")
+	@ApiOperation(value = "Check if a booking exists for a given car registration plate", response = Boolean.class)
+	@ApiResponses(value = {
+	@ApiResponse(code = 200, message = "Request processed successfully"),
+	@ApiResponse(code = 404, message = "Car with provided registration plate not found")
+	})
 	public ResponseEntity<Boolean> isBookingExistsForCar(@PathVariable String registrationPlate) {
 		Car car = carService.getCar(registrationPlate);
 		if (car == null) {
