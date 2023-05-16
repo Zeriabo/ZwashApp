7652c9acc9b4df246fde0c77c08190cb4d6cb1ca
Zeriab Chiah <zeriab@hotmail.com>
2023-05-16 14:18:29 +0300

    firebase initialize

--- src/main/java/com/zwash/ZwashApplication.java
+++ src/main/java/com/zwash/ZwashApplication.java
@@ -2,12 +2,16 @@ package com.zwash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import com.zwash.utility.FirebaseInitializer;
 @SpringBootApplication
 public class ZwashApplication {
 
 	public static void main(String[] args) {
+		
+		
 		SpringApplication.run(ZwashApplication.class, args);
+		
+		FirebaseInitializer.initializeFirebaseApp();
 	}
 
 }
