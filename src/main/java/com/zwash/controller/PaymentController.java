deff6bd426c69c1fbc79ba3b8a8dba39cf3ae03c
Zeriabo <zeriab@hotmail.com>
2023-06-19 22:57:07 +0300

    Payment check out

--- src/main/java/com/zwash/controller/PaymentController.java
+++ src/main/java/com/zwash/controller/PaymentController.java
@@ -1,8 +1,9 @@
 package com.zwash.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,61 +11,42 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.stripe.exception.StripeException;
-import com.stripe.model.checkout.Session;
-import com.stripe.param.checkout.SessionCreateParams;
-import com.zwash.pojos.CarWashingProgram;
-import com.zwash.pojos.Station;
-import com.zwash.service.PaymentService;
-import com.zwash.service.StationService;
-
-import io.swagger.annotations.ApiOperation;
-
 @RestController
 @RequestMapping("/v1/payment")
 public class PaymentController {
-	
 
-	@ApiOperation("creates a checkout session")
-	@PostMapping("/create-checkout-session")
-	public ResponseEntity<?>createCheckoutSession() throws StripeException {
-		
-		String DOMAIN = "http://localhost:7001";
-        SessionCreateParams params =
-          SessionCreateParams.builder()
-            .setMode(SessionCreateParams.Mode.PAYMENT)
-            .setSuccessUrl(DOMAIN + "?success=true")
-            .setCancelUrl(DOMAIN + "?canceled=true")
-            .addLineItem(
-              SessionCreateParams.LineItem.builder()
-                .setQuantity(1L)
-                // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-                .setPrice("{{PRICE_ID}}")
-                .build())
-            .build();
-      Session session = Session.create(params);
-      String sessionUrl = DOMAIN + "/checkout/" + session.getId();  // Replace "/checkout" with your desired URL path
+    @PostMapping("/create-checkout-session")
+    public ResponseEntity<String> createCheckoutSession() throws StripeException {
+        Stripe.apiKey = "sk_test_51NInIUC7hkCZnQICPVg265tvEEClxVcWdBmavlo8LBBtnCjc4VVCtPaegEyry1YJ7pAUCoBuPfmJ8yoQ068uERae001BvwzOiW";
 
-      // Create the ResponseEntity with the redirect URL and HTTP status code
-      ResponseEntity<String> responseEntity = ResponseEntity
-          .status(303)
-          .header("Location",sessionUrl)
-          .body("");
+        String DOMAIN = "http://localhost:7001";
+        SessionCreateParams params = SessionCreateParams.builder()
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl(DOMAIN + "?success=true")
+                .setCancelUrl(DOMAIN + "?canceled=true")
+                .addLineItem(
+                        SessionCreateParams.LineItem.builder()
+                                .setQuantity(1L)
+                                .setPrice("price_1NIp6hC7hkCZnQIC0jzV82ZK")
+                                .build()
+                )
+                .build();
+        Session session = Session.create(params);
+        String sessionUrl = DOMAIN + "/checkout/" + session.getId(); // Replace "/checkout" with your desired URL path
 
-      return responseEntity;
- 
-   
-	}
-	
-	   @ApiOperation("checkout session")
-	   @GetMapping("/checkout/{sessionId}")
-	    public ResponseEntity<String> handleCheckout(@PathVariable String sessionId) {
-	        // Here you can perform any necessary logic for handling the checkout
-	        // process, such as retrieving session details from the database, validating
-	        // the session, and initiating the payment flow.
+        // Create the ResponseEntity with the redirect URL and HTTP status code
+        return ResponseEntity.status(303)
+                .header("Location", sessionUrl)
+                .body("");
+    }
 
-	        // For this example, we'll simply return a success response.
-	        return ResponseEntity.ok("Checkout successful! Session ID: " + sessionId);
-	    }
-	
+    @GetMapping("/checkout/{sessionId}")
+    public ResponseEntity<String> handleCheckout(@PathVariable String sessionId) {
+        // Here you can perform any necessary logic for handling the checkout
+        // process, such as retrieving session details from the database, validating
+        // the session, and initiating the payment flow.
+
+        // For this example, we'll simply return a success response.
+        return ResponseEntity.ok("Checkout successful! Session ID: " + sessionId);
+    }
 }
---
+++ src/main/java/com/zwash/controller/PaymentGatewayController.java
@@ -0,0 +1,43 @@
+package com.zwash.controller;
+
+
+import com.stripe.model.Charge;
+import com.zwash.service.StripeClientService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/payment")
+public class PaymentGatewayController {
+    
+	@Autowired
+    private StripeClientService stripeClient;
+ 
+    PaymentGatewayController() {
+      
+    }
+ 
+    @PostMapping("/charge")
+    public ResponseEntity<String>  chargeCard(@RequestHeader(value="token") String token, @RequestHeader(value="amount") Double amount) throws Exception {
+       Charge succeed = this.stripeClient.chargeNewCard(token, amount);
+       
+       String status = succeed.getStatus();
+       if ("succeeded".equals(status)) {
+           // Payment succeeded
+           // Handle successful payment logic here
+    	   return ResponseEntity.status(303)
+                   .header("Location", status)
+                   .body("");
+       } else {
+           // Payment failed or has a different status
+           // Handle failed payment logic here
+    	   return ResponseEntity.status(500)
+                   .header("Location", status)
+                   .body("");
+       }
+    }
+ 
+
+}
