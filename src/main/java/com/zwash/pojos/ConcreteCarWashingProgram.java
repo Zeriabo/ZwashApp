69e1b2d0b1a86a268a509b1f8fc98e3c51d33f9f
Zeriabo <zeriab@hotmail.com>
2023-07-08 15:05:09 +0300

    Create ConcreteCarWashingProgram.java

---
+++ src/main/java/com/zwash/pojos/ConcreteCarWashingProgram.java
@@ -0,0 +1,67 @@
+package com.zwash.pojos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+public class ConcreteCarWashingProgram {
+	
+	 @JsonProperty(value = "id")
+	   private int id;
+	   
+	   @JsonProperty(value = "price")
+	    private long price;
+
+	   @JsonProperty(value = "description")
+	    private String description;
+	   
+	   @JsonProperty(value = "program")
+	    private String program;
+
+
+	@JsonProperty(value = "updatedAt")
+	   private List<Integer> updatedAt;
+	   @JsonProperty(value = "createdAt")
+	   private List<Integer> createdAt;
+
+	   public List<Integer> getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(List<Integer> updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public List<Integer> getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(List<Integer> createdAt) {
+		this.createdAt = createdAt;
+	}
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+
+}
+
