e64c65c0c67ea2d4d9f53f2d46a1babbd39d53b4
Zeriabo <zeriab@hotmail.com>
2023-08-08 22:04:03 +0300

    Update MediaRepository.java

--- src/main/java/com/zwash/repository/MediaRepository.java
+++ src/main/java/com/zwash/repository/MediaRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import com.zwash.pojos.Media;
 
-@Repository
+
 public interface MediaRepository extends JpaRepository<Media, Long> {
     // Custom query methods can be defined here if needed
 }
