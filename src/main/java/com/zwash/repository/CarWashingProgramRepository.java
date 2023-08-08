37f9148a203c0c126dbf73e8e0e1a88f95ab1b98
Zeriabo <zeriab@hotmail.com>
2023-08-08 22:04:11 +0300

    Update CarWashingProgramRepository.java

--- src/main/java/com/zwash/repository/CarWashingProgramRepository.java
+++ src/main/java/com/zwash/repository/CarWashingProgramRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import com.zwash.pojos.CarWashingProgram;
 
-@Repository
+
 public interface CarWashingProgramRepository extends JpaRepository<CarWashingProgram, Long> {
 
 
