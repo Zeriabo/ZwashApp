5ed998b6fce583e6de35481ce6749991e95ba8ee
Zeriabo <zeriab@hotmail.com>
2023-09-01 14:34:13 +0300

    Service provider Pojos

---
+++ src/main/java/com/zwash/pojos/ServiceProvider.java
@@ -0,0 +1,64 @@
+package com.zwash.pojos;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "service_provider")
+public class ServiceProvider {
+
+  
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Station> getStations() {
+		return stations;
+	}
+
+	public void setStations(List<Station> stations) {
+		this.stations = stations;
+	}
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+   @Column(nullable = false)
+   private String name;
+   
+   @Column(nullable = false)
+   private String email;
+   
+   @OneToMany(mappedBy = "serviceProvider", cascade = CascadeType.ALL)
+   private List<Station> stations;
+
+}
