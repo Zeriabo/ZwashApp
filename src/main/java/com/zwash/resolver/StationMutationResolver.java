710f3e83f1f13b65c3f2d333fe21c6f198246261
Zeriabo <zeriab@hotmail.com>
2023-09-07 16:15:07 +0300

    Update StationMutationResolver.java

--- src/main/java/com/zwash/resolver/StationMutationResolver.java
+++ src/main/java/com/zwash/resolver/StationMutationResolver.java
@@ -1,9 +1,12 @@
 package com.zwash.resolver;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.zwash.dto.StationDTO;
+import com.zwash.pojos.CarWashingProgram;
 import com.zwash.pojos.Station;
 import com.zwash.service.StationService;
 
@@ -13,46 +16,68 @@ import graphql.kickstart.tools.GraphQLMutationResolver;
 public class StationMutationResolver implements GraphQLMutationResolver {
 
 	@Autowired
-    StationService stationService;
+	StationService stationService;
 
-    @Autowired
-    public StationMutationResolver(StationService stationService) {
-        this.stationService = stationService;
-    }
+	@Autowired
+	public StationMutationResolver(StationService stationService) {
+		this.stationService = stationService;
+	}
 
-    public Station createStation(Station stationInput) throws Exception {
-        // Implement your logic to create a booking using the bookingInput data
-        StationDTO station =new StationDTO();
-        // Set other properties of the booking
+	public Station createStation(Station stationInput) throws Exception {
+		// Implement your logic to create a booking using the bookingInput data
+		StationDTO station = new StationDTO();
+		// Set other properties of the booking
 
-        // Save the booking using the service
-        return stationService.createStation(stationInput);
-    }
+		// Save the booking using the service
+		return stationService.createStation(stationInput);
+	}
 
-    public Station updateStation(Long id, Station station) throws Exception {
-        // Retrieve the existing booking by id
-        Station oldStation = stationService.getStation(id);
-        if (oldStation == null) {
-            throw new IllegalArgumentException("oldStation not found");
-        }
+	
+	public Station createStationWithPrograms(Station stationInput) throws Exception {
 
-        // Update properties of the existing booking using bookingInput
-        // For example: existingBooking.set... (set properties from bookingInput)
-            Station newStation = null;
-        // Save the updated booking using the service
-        return stationService.createStation(newStation);
-    }
+		return stationService.createStation(stationInput);
+	}
 
-    public Boolean deleteStation(Long id) throws Exception {
-        // Retrieve the booking by id
-        Station  station = stationService.getStation(id);
-        if (station == null) {
-            throw new IllegalArgumentException("station not found");
-        }
+	public Station createStationWithoutPrograms(Station stationInput) throws Exception {
 
-        // Delete the booking using the service
-        stationService.removeStation(id);
+		return stationService.createStation(stationInput);
+	}
 
-        return true;
-    }
+	public Station updateCarWashPrograms(Long stationId, List<CarWashingProgram> washingPrograms) throws Exception {
+		// Retrieve the existing booking by id
+		Station station = stationService.getStation(stationId);
+		if (station == null) {
+			throw new IllegalArgumentException("station not found");
+		}
+		station.setPrograms(washingPrograms);
+		// Save the updated booking using the service
+		return stationService.createStation(station);
+	}
+
+	public Station updateStation(Long id, Station station) throws Exception {
+		// Retrieve the existing booking by id
+		Station oldStation = stationService.getStation(id);
+		if (oldStation == null) {
+			throw new IllegalArgumentException("oldStation not found");
+		}
+
+		// Update properties of the existing booking using bookingInput
+		// For example: existingBooking.set... (set properties from bookingInput)
+		Station newStation = null;
+		// Save the updated booking using the service
+		return stationService.createStation(newStation);
+	}
+
+	public Boolean deleteStation(Long id) throws Exception {
+		// Retrieve the booking by id
+		Station station = stationService.getStation(id);
+		if (station == null) {
+			throw new IllegalArgumentException("station not found");
+		}
+
+		// Delete the booking using the service
+		stationService.removeStation(id);
+
+		return true;
+	}
 }
